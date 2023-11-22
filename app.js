221059b2f897115a8693dd3e6f66c44c1f816d89
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-11-22 16:54:20 +0100

    Add navbar menus styling and functionality

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log('It works');
+const menuButtons = [...document.querySelectorAll('[aria-controls]')];
+const notificationButton = menuButtons.find(
+  (menuButton) =>
+    menuButton.getAttribute('aria-controls') === 'notification-menu'
+);
+const collectionsButton = menuButtons.find(
+  (menuButton) =>
+    menuButton.getAttribute('aria-controls') === 'collections-menu'
+);
+
+function closeOtherMenuBtns(currentMenuId) {
+  const otherMenuBtns = menuButtons.filter(
+    (menuButton) => menuButton.getAttribute('aria-controls') !== currentMenuId
+  );
+  otherMenuBtns.forEach((menuButton) => {
+    const menuId = menuButton.getAttribute('aria-controls');
+    const menu = document.getElementById(menuId);
+    menuButton.setAttribute('aria-expanded', false);
+    menu.setAttribute('aria-hidden', true);
+  });
+}
+
+function toggleMenu(event) {
+  const button = event.currentTarget;
+  const menuId = button.getAttribute('aria-controls');
+  const menu = document.getElementById(menuId);
+  if (button.getAttribute('aria-expanded') === 'false') {
+    button.setAttribute('aria-expanded', true);
+    menu.setAttribute('aria-hidden', false);
+    closeOtherMenuBtns(menuId);
+  } else {
+    button.setAttribute('aria-expanded', false);
+    menu.setAttribute('aria-hidden', true);
+  }
+}
+
+function closeAllMenus(event) {
+  if (event.target.closest('[aria-controls]')) return;
+  menuButtons.forEach((menuButton) => {
+    if (menuButton.getAttribute('aria-expanded') === 'true') {
+      const menuId = menuButton.getAttribute('aria-controls');
+      const menu = document.getElementById(menuId);
+      menuButton.setAttribute('aria-expanded', false);
+      menu.setAttribute('aria-hidden', true);
+    }
+  });
+}
+
+notificationButton.addEventListener('click', toggleMenu);
+collectionsButton.addEventListener('click', toggleMenu);
+document.addEventListener('click', closeAllMenus);
