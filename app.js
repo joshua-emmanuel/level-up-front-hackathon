c7d1294659a6e7035cab6eb58a205439e2f11667
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-11-24 21:23:07 +0100

    Add functionality to open and close accordions and to update progress bar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const menuButtons = [...document.querySelectorAll('[aria-controls]')];
+const menuButtons = [...document.querySelectorAll('[aria-haspopup="menu"]')];
 const notificationButton = menuButtons.find(
   (menuButton) =>
     menuButton.getAttribute('aria-controls') === 'notification-menu'
@@ -10,6 +10,15 @@ const collectionsButton = menuButtons.find(
 const trialCalloutDismissBtn = document.querySelector(
   '.trial-callout__dismiss-btn'
 );
+const setupStepsAccordionBtn = document.querySelector(
+  '[aria-controls="setup-guide__steps"]'
+);
+const setupGuideSteps = document.querySelector('.setup-guide__steps');
+const progressBar = document.querySelector('.progress-bar');
+const numOfCompletedGuides = document.querySelector('.no-of-completed-guide');
+const allSetupGuideAccordionCheckbox = [
+  ...document.querySelectorAll('.setup-guide__checkbox'),
+];
 
 function closeOtherMenuBtns(currentMenuId) {
   const otherMenuBtns = menuButtons.filter(
@@ -59,7 +68,98 @@ function dismissTrialCallout(event) {
   trialCallout.remove();
 }
 
+function openAccordion(accordionButton, accordionContent) {
+  accordionContent.setAttribute('aria-hidden', 'false');
+  accordionButton.setAttribute('aria-expanded', 'true');
+}
+
+function closeAccordion(accordionButton, accordionContent) {
+  accordionContent.setAttribute('aria-hidden', 'true');
+  accordionButton.setAttribute('aria-expanded', 'false');
+}
+
+function toggleAccordion(accordionButton, accordionContent) {
+  if (accordionContent.getAttribute('aria-hidden') === 'true') {
+    openAccordion(accordionButton, accordionContent);
+  } else {
+    closeAccordion(accordionButton, accordionContent);
+  }
+}
+
+function toggleSetupStepsAccordion(event) {
+  const accordionButton = event.currentTarget;
+  const accordionContentId = accordionButton.getAttribute('aria-controls');
+  const accordionContent = document.getElementById(accordionContentId);
+  toggleAccordion(accordionButton, accordionContent);
+}
+
+function closeAllSetupGuideStepsAccordions() {
+  const allSetupGuideAccordionBtns = [
+    ...setupGuideSteps.querySelectorAll('button[aria-controls]'),
+  ];
+  allSetupGuideAccordionBtns.forEach((accBtn) => {
+    const accContentId = accBtn.getAttribute('aria-controls');
+    const accContent = document.getElementById(accContentId);
+    closeAccordion(accBtn, accContent);
+  });
+}
+
+function toggleSetupGuideStepsAccordion(event) {
+  const accordionButton = event.target.closest('button[aria-controls]');
+  if (!accordionButton) return;
+  const accordionContentId = accordionButton.getAttribute('aria-controls');
+  const accordionContent = document.getElementById(accordionContentId);
+  closeAllSetupGuideStepsAccordions();
+  openAccordion(accordionButton, accordionContent);
+}
+
+function openNextSetupGuideStepsAccordion(event) {
+  const setupGuideCheckbox = event.target.closest('.setup-guide__checkbox');
+  if (!setupGuideCheckbox) return;
+  const checkboxIsChecked = setupGuideCheckbox.checked;
+  if (!checkboxIsChecked) return;
+  const currentAccordion = setupGuideCheckbox.closest('.setup-guide__step');
+  const currentAccordionLabel =
+    currentAccordion.getAttribute('aria-labelledby');
+  const nextAccordions = [
+    ...setupGuideSteps.querySelectorAll(
+      `[aria-labelledby="${currentAccordionLabel}"] ~ .setup-guide__step`
+    ),
+  ];
+  const accordionToBeOpened = nextAccordions.find((accordion) => {
+    const accordionCheckbox = accordion.querySelector('.setup-guide__checkbox');
+    if (!accordionCheckbox.checked) return true;
+  });
+  if (!accordionToBeOpened) return;
+  const accordionToBeOpenedButton = accordionToBeOpened.querySelector(
+    'button[aria-controls]'
+  );
+  const accordionToBeOpenedContentId =
+    accordionToBeOpenedButton.getAttribute('aria-controls');
+  const accordionToBeOpenedContent = document.getElementById(
+    accordionToBeOpenedContentId
+  );
+  closeAllSetupGuideStepsAccordions();
+  openAccordion(accordionToBeOpenedButton, accordionToBeOpenedContent);
+}
+
+function updateProgressBar(event) {
+  if (!event.target.closest('.setup-guide__checkbox')) return;
+  const checkedCheckboxes = allSetupGuideAccordionCheckbox.filter(
+    (checkbox) => checkbox.checked
+  );
+  const numofCheckedCheckboxes = checkedCheckboxes.length;
+  const percentageOfCheckedCheckboxes =
+    (numofCheckedCheckboxes / allSetupGuideAccordionCheckbox.length) * 100;
+  progressBar.style.width = `${percentageOfCheckedCheckboxes}%`;
+  numOfCompletedGuides.textContent = numofCheckedCheckboxes;
+}
+
 notificationButton.addEventListener('click', toggleMenu);
 collectionsButton.addEventListener('click', toggleMenu);
 document.addEventListener('click', closeAllMenus);
 trialCalloutDismissBtn.addEventListener('click', dismissTrialCallout);
+setupStepsAccordionBtn.addEventListener('click', toggleSetupStepsAccordion);
+setupGuideSteps.addEventListener('click', toggleSetupGuideStepsAccordion);
+setupGuideSteps.addEventListener('click', openNextSetupGuideStepsAccordion);
+setupGuideSteps.addEventListener('click', updateProgressBar);
